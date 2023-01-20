fc81dc84c3135d98934ce9f91081303f28b3a919
vpershko07 <vpershko07@gmail.com>
2023-01-20 09:56:49 -0500

    Add files via upload

--- gen_email.py
+++ gen_email.py
@@ -4,9 +4,30 @@ from consts import DB_PATH
 from db import get_db_connection
 from repository import T2Repository
 import hashlib
+from load_tweepy_api import get_tweepy_api
 
 def sha1_key(t2handle):
     return hashlib.sha1(f"{t2handle}7e##$83R73df1bFf3C9548".encode('utf-8')).hexdigest()
+
+
+def rot5_encode(string):
+    encoded_string = ""
+    for char in string:
+        if char.isdigit():
+            encoded_string += chr((ord(char) - ord('0') + 5) % 10 + ord('0'))
+        else:
+            encoded_string += char
+    return encoded_string
+
+
+def get_user_profile(twitter_handle, api):
+    """ gets the user twitter object """
+    try:
+        return api.get_user(screen_name = twitter_handle,include_entities= False)
+    except Exception as e :
+        print(f"[ERROR] handle {twitter_handle}, becuase of {e}")
+        return e 
+
 
 
 @click.command(help="t2 handle")
@@ -14,16 +35,25 @@ def sha1_key(t2handle):
 @click.option("--twitterhandle", help="twitter handle")
 def main(t2handle, twitterhandle):
     repository = T2Repository(get_db_connection(DB_PATH))
-
+    
     # checking if user exists or not 
     if not repository.is_user_profile_exist(twitterhandle):
-        print("[ERROR] THIS USER PROFILE DOESNOT EXIST")
-        return
+        print("[WARNING] THIS USER PROFILE DOESNOT EXIST")
+        print("[INFO] CRAWLING TWITTER PROFILE")
+        twitter_api  = get_tweepy_api()
+        user_profile =  get_user_profile(twitterhandle, twitter_api)
+        first_name = user_profile._json['name']
+        id = str(user_profile._json['id'])
+        repository.insert_user_profile(user_profile._json)
+
+    else:
+        id = repository.get_user_profile_id(twitter_handle=twitterhandle)
+        first_name = repository.get_name_by_twitter_username(twitterhandle)['name']
 
     PH_SEE_BELOW  = codecs.encode(twitterhandle, 'rot_13') # rot13 encoding for twitter handle 
-    PID_SEE_BELOW = int(repository.get_user_profile_id(twitter_handle=twitterhandle))*3
+    # PID_SEE_BELOW = int(repository.get_user_profile_id(twitter_handle=twitterhandle))*3
+    PID_SEE_BELOW = rot5_encode(str(id))
     KEY_SEE_BELOW = sha1_key(t2handle) # sha1 key for the t2handle
-    first_name = repository.get_name_by_twitter_username(twitterhandle)['name']
 
     try:
         recommendation_newwatlist_connectons = repository.find_recommondation_new_waitlist_connection(twitterhandle)[0]['t2_usernames'].split(',')
@@ -45,7 +75,14 @@ def main(t2handle, twitterhandle):
 
     list_of_recommendations = [f"https://t2.social/{t2_handle}" for t2_handle in recommendation_newwatlist_connectons]
     list_of_recommendations.extend([f"https://t2.social/{t2_handle}" for t2_handle in recommendation_connectivity_data if t2_handle not in recommendation_newwatlist_connectons])
-    list_of_recommendations_text = '\n'.join(list_of_recommendations)
+    
+    # check whether there is a connections or not.
+    if not list_of_recommendations:
+        pre_recommendations_list_text = ""
+        list_of_recommendations_text = ""
+    else:
+        pre_recommendations_list_text = "Once you’ve signed up here are some recommendations for you for people to follow:"
+        list_of_recommendations_text = '\n'.join(list_of_recommendations)
 
     message = f"""   
 Subject: T2 Invite: @{t2handle}
@@ -54,7 +91,7 @@ Subject: T2 Invite: @{t2handle}
                 
 Really excited to welcome you to T2. Here is your invite to claim handle @{t2handle}:
 https://t2.social/signup?handle={t2handle}&ph={PH_SEE_BELOW}&pid={PID_SEE_BELOW}&key={KEY_SEE_BELOW}
-Once you've signed up here are some recommendations for you for people to follow:
+{pre_recommendations_list_text}
 {list_of_recommendations_text}
 
 Thanks for joining!
---
+++ username_crawler.py
@@ -0,0 +1,92 @@
+from consts import DB_PATH
+from db import get_db_connection
+from repository import T2Repository
+from load_tweepy_api import get_tweepy_api
+import tweepy
+import click
+
+repository = T2Repository(get_db_connection(DB_PATH))
+
+
+def process_page(page, twitter_handle, twitter_id,friend):
+    """ Entering every user to the DB. """
+    counter = 0
+    if friend:
+        for user in page:
+            repository.insert_to_new_waitlist_connections(user._json['screen_name'], user._json['id'],twitter_handle,twitter_id)
+            counter += 1
+    else:
+        for user in page:
+            repository.insert_to_new_waitlist_connections(twitter_handle,twitter_id,user._json['screen_name'], user._json['id'])
+            counter +=1
+    
+    print(f"[INFO] PAGE HAS {counter} OF USERS")
+
+
+def crawl(twitter_handle,twitter_id,api):
+    """ Crawling user friends/followers """
+    try:
+        for idx, page in enumerate(tweepy.Cursor(api.get_friends, screen_name  = twitter_handle, skip_status= True, include_user_entities= False, count = 200).pages()):
+            process_page(page, twitter_handle, twitter_id, friend=True)
+            print(f"[INFO] IN HANDLE {twitter_handle}, IN PAGE NUMBER {idx} OF FRIENDS")
+
+        for idx, page in enumerate(tweepy.Cursor(api.get_followers, screen_name = twitter_handle, skip_status=True, include_user_entities=False, count = 200).pages()):
+            process_page(page, twitter_handle, twitter_id, friend=False)
+            print(f"[INFO] IN HANDLE {twitter_handle}, IN PAGE NUMBER {idx} OF FOLLOWING")
+
+        repository.insert_to_already_crawled(twitter_handle,twitter_id)
+
+    except Exception as e:
+        print(f"[ERROR] ERROR IN HANDLE {twitter_handle}; {e}")
+
+
+
+def get_user_profile(twitter_handle, api):
+    """ gets the user twitter object """
+    try:
+        return api.get_user(screen_name = twitter_handle,include_entities= False)
+    except Exception as e :
+        print(f"[ERROR] handle {twitter_handle}, becuase of {e}")
+        return e 
+
+
+
+@click.command(help="twitter handle crawler")
+@click.option("--twitterhandle", type=str, help="twitter handle")
+@click.option("--connections", default=False, type=bool, help="get twitter connections")
+def main(twitterhandle, connections):
+    api = get_tweepy_api()
+    
+    try:
+        waiter_handle = twitterhandle.strip()
+        print(f"[INFO] IN HANDLE: {waiter_handle}")
+
+        if waiter_handle is None or waiter_handle == "":
+            print(f"[INFO] NO TWITTER HANDLE:: {waiter_handle}")
+            return
+
+        if not repository.is_user_profile_exist(waiter_handle) and waiter_handle is not None:
+            print("[INFO] GETTING USER PROFILE..")
+            user = get_user_profile(waiter_handle, api) # getting the user object from tweepy api.
+            repository.insert_user_profile(user._json) # inserting the user into twitter_profile DB.
+            print("[INFO] USER INSERTED TO DATABSE.")
+        else:
+            print(f"[INFO] HANDLE {waiter_handle} ALREADY EXISTS IN TWIITER PROFILES TABLE")
+
+        # Get connectivity data if flag is True.
+        if connections:
+            if (not repository.is_twitter_handle_crawled(waiter_handle)) and waiter_handle is not None:
+                waiter_id = repository.get_user_profile_id(waiter_handle)
+                if waiter_id is None:
+                    waiter_id = user.__json['id']
+                print("[INFO] CRAWLING USER REALTIONSHIPS..")
+                crawl(waiter_handle,waiter_id,api)
+            else:
+                print(f"[INFO] HANDLE {waiter_handle} ALREADY CRAWLED")
+
+    except Exception as e :
+        print(f"[ERROR] {e}")
+
+
+if __name__ == "__main__":
+    main()
