3784ecd89a5d1726b67b5abaf229318a8f55490d
vpershko07 <vpershko07@gmail.com>
2023-02-06 09:34:11 -0500

    Add files via upload

--- next_most_connected.py
+++ next_most_connected.py
@@ -11,10 +11,13 @@ repository = T2Repository(get_db_connection(DB_PATH))
 @click.option("--limit", default=200, help="Limit the number of users to return")
 def main(limit):
     best_candidates = repository.find_next_most_connected_users_on_wait_list_updated_v2(limit)
-    # print("The next most connected users on the waitlist are:")
+    print("The next most connected users on the waitlist are:")
     for candidate in best_candidates:
-        list_of_targets = [f"https://t2.social/{target}" for target in candidate['target_handles'].split(',')]
-        print(f"candidate: {candidate['user_twitter_handle']}, conneceted to: {list_of_targets}")
+        targets = candidate['T2_handle'].split(',')
+        print(f"Candidate: {candidate['user_twitter_handle']}, conneceted to:", end= "")
+        for target in targets:
+            print(f"https://t2.social/{target}", end=", ")
+        print("\n")
 
 
 if __name__ == "__main__":
